f7c9455add78d77bf944f18f5ab0c49b46bd8871
dlucasclua <dlucasclua@gmail.com>
2024-04-22 09:33:37 +0200

    res2

--- src/main.py
+++ src/main.py
@@ -172,12 +172,12 @@ async def main(page_url, filename) -> None:
                 Actor.log.info('Ja he trobat el button_aceptar')
                 # button_aceptar = page.locator('/html/body/div[1]/div/div/div/div/div[2]/button[3]')
                 await asyncio.sleep(10)
-<<<<<<< HEAD
+
                 await button_aceptar.click()
 
-=======
+
                 button_aceptar.click()
->>>>>>> 41b0c7af14251a2c6670d67f9a6e841d64e5a1c1
+
                 #page.get_by_role('button', name='Aceptar y continuar').click()
                 Actor.log.info('Ja he fet click() al button_aceptar')
 
